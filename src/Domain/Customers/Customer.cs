21ea034f0ccd136c7b11519166adf02f98874f02
Willy Tjandra <willy.tjandra@gmail.com>
2023-07-24 09:51:49 +1000

    add Customer domain

---
+++ src/Domain/Customers/Customer.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain.Customers;
+
+public class Customer
+{
+	private Customer()
+	{
+	}
+
+	public CustomerId Id { get; private set; }
+
+	public string FirstName { get; private set; }
+
+	public string LastName { get; private set; }
+
+	public string Email { get; private set; }
+
+	public UserStatus Status { get; private set; }
+
+	public static Customer New(string firstName, string lastName, string email)
+	{
+		return new Customer
+		{
+			Id = new CustomerId(Guid.NewGuid()),
+			FirstName = firstName,
+			LastName = lastName,
+			Email = email,
+		};
+	}
+}
---
+++ src/Domain/Customers/UserStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Customers;
+
+public enum UserStatus
+{
+	Active,
+	Inactive,
+}
